fb9a8afcf9629266e1d5455268f1a8485c8a5bd8
NAYEM ISLAM <gangsn490@gmail.com>
2024-12-06 11:00:56 +0600

    fix: add router in testing purpose

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// IMPORT ROUTER NPM PACKAGE
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+
+import './index.css';
+
+// IMPORT PAGES
+import App from './App.jsx';
+import Home from './Pages/Home/Home.jsx';
+import Blogs from './Pages/Blogs/Blogs.jsx';
+
+// this setup only testing purpose using //
+// SETUP ROUTER
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} element={<Home />} />
+      <Route path='/blogs' element={<Blogs />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
